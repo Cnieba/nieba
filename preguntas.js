950ac4c6f5a75059a7c8533c8579467d8ac30c83
Cnieba <142542075+Cnieba@users.noreply.github.com>
2023-11-28 18:48:18 -0300

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "¿Cuál es el lenguaje estándar específico para aplicar estilos de presentación a nuestras páginas web?",
+    answer: "CSS",
+    options: [
+      "Javascript",
+      "CSS",
+      "PHP",
+      "Flash"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Que atributo sirve para insetar imágenes?:",
+    answer: "Scr",
+    options: [
+      "Id",
+      "Ul",
+      "Href",
+      "Scr"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Qué atributo de HTML se emplea en un formulario para especificar la página a la que se van a enviar los datos del mismo?",
+    answer: "Action",
+    options: [
+      "File",
+      "Method",
+      "Action",
+      "Name"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Qué significa CSS?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Color Style Sheets",
+      "Cascading Style Sheets",
+      "Creative Style Sheets",
+      " Computer Style Sheets"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Que etiqueta difine la división o una sección en un documento HTML?",
+    answer: "Div",
+    options: [
+      "Div",
+      "Span",
+      "P",
+      "Li"
+    ]
+  },
+  ];
